97e62e80fe0c470edbfc75374d3818beec2fde7f
nruest <ruestn@gmail.com>
2017-07-09 15:22:26 -0400

    🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭🌭

--- dplafy.py
+++ dplafy.py
@@ -11,7 +11,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-dpla_data = "http://api.dp.la/v2/items?q=%22donald+trump%22+OR+motorcycle+OR+%22harley+davidson%22+OR+%22chicken+wings%22+OR+turkey+OR+fries+OR+hamburger+OR+hotdog+OR+eagle+OR+%22american+flag%22+OR+beer+OR+beef+OR+war+OR+cow+OR+explosion+OR+explosive+OR+gun+OR+chrome+OR+%22all+you+can+eat%22&page_size=500&api_key=91b1bb54a822c576aa55d1b7a9babbd6"
+dpla_data = "http://api.dp.la/v2/items?sourceResource.description=%22chicken+wings%22+OR+fries+OR+hamburger+OR+hotdog+OR+eagle+OR+beer+OR+beef++OR+%22all+you+can+eat%22&page_size=500&api_key=91b1bb54a822c576aa55d1b7a9babbd6"
 
 response = urllib.urlopen(dpla_data);
 data = json.loads(response.read())
@@ -21,12 +21,13 @@ items = random.sample(docs,1)
 
 for item in items:
   url = "https://dp.la/item/" + item["id"]
-  description = item["sourceResource"]["title"][0]
-  fuck_yeah = "#muricafuckyeah"
+  description = item["sourceResource"]["description"][0]
   description = (description[:102] + '...') if len(description) > 102 else description
   if len(description) <= 1:
       break
   us_flag = u'\U0001F1FA\U0001F1F8'
-  description = description + "\n" + " " + us_flag + " " + fuck_yeah
+  hotdog = u'\U0001F32D'
+  eagle = u'\U0001F985'
+  description = description + "\n" + " " + hotdog + " " + eagle + " " + us_flag
   tweet_text = "%s %s" % (description,url)
   api.update_status(tweet_text)
